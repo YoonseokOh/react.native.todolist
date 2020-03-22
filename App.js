8a0750ac49b1c4bffccd1f9ecb8495f42e8ba5e4
Yoonseok Oh <promiseys@hotmail.com>
2020-03-22 21:32:09 +0900

    First commit

---
+++ App.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {
+   StyleSheet,
+   FlatList,
+   SafeAreaView,
+   AsyncStorage,
+} from 'react-native';
+import Header from './components/Header'
+import TodoItem from './components/TodoItem'
+import TaskModal from './components/TaskModal'
+
+export default class App extends React.Component {
+  state = {
+    todos: [{
+      title: '클래스101 리액트 네이티브',
+      done: true,
+    }, {
+      title: '클래스101 에프터 이펙트',
+      done: false,
+    }],
+    showModal: false,
+  }
+  
+  UNSAFE_componentWillMount() {
+    AsyncStorage.getItem('@todo:state').then((state) => {
+      this.setState(JSON.parse(state))
+    })
+  }
+
+  save = () => {
+    AsyncStorage.setItem('@todo:state', JSON.stringify(this.state))
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header
+          show={() => {
+            this.setState({ showModal: true })
+          }}
+        />
+        <FlatList
+          data={this.state.todos}
+          renderItem={({ item, index }) => {
+            return (
+              <TodoItem
+                title={item.title}
+                done={item.done}
+                remove={() => {
+                  this.setState({
+                    todos: this.state.todos.filter((_, i) => i !== index)
+                  }),
+                  this.save
+                }}
+                toggle={() => {
+                  const newTodos = [...this.state.todos]
+                  newTodos[index].done = !newTodos[index].done
+                  this.setState({
+                    todos: newTodos
+                  }),
+                  this.save
+                }}
+              />
+            )
+          }}
+          keyExtractor={(_, index) => {
+            return `${index}`
+          }}
+        />
+        <TaskModal
+          isVisible={this.state.showModal}
+          add={(title) => {
+            this.setState({
+              todos: this.state.todos.concat({
+                title: title,
+                done: false,
+              }),
+              showModal: false
+            }),
+            this.save
+          }}
+          hide={() => {
+            this.setState({ showModal: false })
+          }}
+        />
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
